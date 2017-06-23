eb5a397197a21bcfbb68a1dd9f79edd86a46baf5
Daniel Szabo <garfailed@users.noreply.github.com>
2017-06-23 11:10:59 +0200

    Add authorization header constant.

--- src/main/java/com/ziffit/autoconfigure/spring/security/SecurityConstants.java
+++ src/main/java/com/ziffit/autoconfigure/spring/security/SecurityConstants.java
@@ -2,6 +2,8 @@ package com.ziffit.autoconfigure.spring.security;
 
 public class SecurityConstants {
 
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
     public static final String ROLE_ANONYMOUS = "ROLE_ANONYMOUS";
     public static final String ROLE_USER = "ROLE_USER";
 
