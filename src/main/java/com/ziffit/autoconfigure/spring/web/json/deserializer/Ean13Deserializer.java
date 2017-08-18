1e14050328a8388b5e532130a5faf1dfabc38966
Daniel Szabo <garfailed@users.noreply.github.com>
2017-08-18 16:30:05 +0200

    Remove barcode formatter. Add Jackson field deserializer.

--- src/main/java/com/ziffit/autoconfigure/spring/web/json/deserializer/Ean13Deserializer.java
+++ src/main/java/com/ziffit/autoconfigure/spring/web/json/deserializer/Ean13Deserializer.java
@@ -1,55 +1,51 @@
-package com.ziffit.autoconfigure.spring.web.formatting.fieldannotation.barcode;
+package com.ziffit.autoconfigure.spring.web.json.deserializer;
 
-import java.text.ParseException;
-import java.util.Locale;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 
-import org.springframework.format.Formatter;
+import java.io.IOException;
 
-public class BarcodeFormatter implements Formatter<String> {
-    
+public class Ean13Deserializer extends JsonDeserializer<String> {
+
     @Override
-    public String parse(String barcode, Locale locale) throws ParseException {
-        if(barcode.matches("[0123456789Xx]+")) {
+    public String deserialize(JsonParser parser, DeserializationContext context) throws IOException {
+        String barcode = parser.getValueAsString();
+        if (barcode.matches("[0123456789Xx]+")) {
             if (barcode.length() == 9) {
                 barcode = "0" + barcode;
             }
-    
+
             if (barcode.length() == 10) {
                 barcode = formatISBN10(barcode);
             } else if (barcode.length() == 12) {
                 barcode = formatUPC12(barcode);
             }
         }
-        
+
         return barcode;
     }
-    
-    
-    @Override
-    public String print(String barcode, Locale locale) {
-        return barcode;
-    }
-    
+
     private String formatUPC12(String barcode) {
         return "0" + barcode;
     }
-    
+
     private String formatISBN10(String barcode) {
         String substring = barcode.substring(0, 9);
-    
+
         String ean13 = "978" + substring;
-    
+
         int result = 0;
         for (int i = 0; i < ean13.length(); i++) {
             result += Integer.parseInt(Character.toString(ean13.charAt(i))) * ((i % 2 == 0) ? 1 : 3);
         }
-    
+
         int checkDigit = 10 - (result % 10);
         if (checkDigit == 10) {
             checkDigit = 0;
         }
         ean13 += checkDigit;
-    
+
         return ean13;
     }
 }
