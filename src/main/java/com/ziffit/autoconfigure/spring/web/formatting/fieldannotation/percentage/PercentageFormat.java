34710c50230c4bd5648243bcf065bc18db5f944c
Daniel Szabo <garfailed@users.noreply.github.com>
2017-10-09 22:13:29 +0200

    Add JavaDoc comment.

--- src/main/java/com/ziffit/autoconfigure/spring/web/formatting/fieldannotation/percentage/PercentageFormat.java
+++ src/main/java/com/ziffit/autoconfigure/spring/web/formatting/fieldannotation/percentage/PercentageFormat.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This formatter is useful when percentages arrive in an easily readable
+ * format, such as 25%, but we want to store them in their decimal format, such as 0.25.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface PercentageFormat {
